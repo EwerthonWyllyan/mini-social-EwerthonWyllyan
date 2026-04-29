45174a4b94c658718037be1f444dd0baa39d77d2
EwerthonWyllyan <ewerthon.7565008@aluno.mg.gov.br>
2026-04-29 16:03:55 +0000

    você add confirmadore

--- js/script.js
+++ js/script.js
@@ -1,35 +1,63 @@
+//=== ESTADO (dados da aplicação) === 
+
 let likeCount = 0;
-let curtido = false; // booleana
-let deslikeCount = 0;
-let descurtido = false; // booleana
+let dislikeCount = 0;
+let curtido = false;
+let descurtido = false;
+
+//=== SERVICE (regras de negócio) ===
 
 function curtir() {
- if(curtido == false){
-   likeCount++;
-   curtido = true;
-  document.getElementById("likeCount").innerText = likeCount;
-} else{
-  likeCount--;
-   curtido = false;
-  document.getElementById("likeCount").innerText = likeCount;
+  if (curtido == false){
+    likeCount++;
+    curtido = true;
+    document.getElementById("likeCount").innerText = likeCount;
+
+    if(descurtido == true){
+      dislikeCount--;
+      descurtido = false;
+      document.getElementById("dislikeCount").innerText = dislikeCount;
+
+    }
+
+  }else{
+    likeCount--;
+    curtido = false;
+    document.getElementById("likeCount").innerText = likeCount;
+  }
+
 }
-
- }
-
-
-
 
 function descurtir() {
- if(deslike == false){
-   deslikeCount++;
-   deslike = true;
-  document.getElementById("deslikeCount").innerText = deslikeCount;
-} else{
-  deslikeCount--;
-   deslike = false;
-  document.getElementById("deslikeCount").innerText = deslikeCount;
+  if(descurtido == false){
+    dislikeCount++;
+    descurtido = true;
+    document.getElementById("dislikeCount").innerText = dislikeCount;
+
+    if(curtido == true){
+      likeCount--;
+      curtido = false;
+      document.getElementById("likeCount").innerText = likeCount;
+    }
+
+  }
+  else{
+    dislikeCount--;
+    descurtido = false;
+    document.getElementById("dislikeCount").innerText = dislikeCount;
+  }
 }
 
- }
-document.getElementById("likeBtn").addEventListener("click", curtir);
-document.getElementById("deslikeBtn").addEventListener("click", descurtir);
+//==== CONTROLER (intermediação)===
+
+function cilcarCurtir(){
+  curtir();
+}
+function cilcarDescurtir(){
+  curtir();
+}
+
+// ===  EVENTOS ===
+
+document.getElementById("likeBtn").addEventListener("click", cilcarCurtir);
+document.getElementById("dislikeBtn").addEventListener("click", cilcarDescurtir);
